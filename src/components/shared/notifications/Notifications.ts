2c14aab86a889b0043fa006a028bc9aeb0032903
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-03-19 20:54:38 +0100

    clean comments

--- src/components/shared/notifications/Notifications.ts
+++ src/components/shared/notifications/Notifications.ts
@@ -1,6 +1,5 @@
 import { toast } from 'react-toastify';
 
-// Функция для успешного уведомления
 export const notifySuccess = (message: string): void => {
   toast.success(message, {
     position: 'top-right',
@@ -13,7 +12,6 @@ export const notifySuccess = (message: string): void => {
   });
 };
 
-// Функция для уведомления об ошибке
 export const notifyError = (message: string): void => {
   toast.error(message, {
     position: 'top-right',
